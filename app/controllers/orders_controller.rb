4c1f3284105485223d942579c69ce2b8735b1639
Akash <akash.singh@neosoftmail.com>
2022-06-09 11:15:02 +0530

    address_id added to order table.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -20,6 +20,7 @@ class OrdersController < ApplicationController
     end
     @order = Order.new
     @total = params[:total]
+    @address_id = params[:address_id]
   end
 
   # GET /orders/1/edit
@@ -29,41 +30,32 @@ class OrdersController < ApplicationController
   # POST /orders or /orders.json
   def create
     total = params["order"][:total]
+    address_id = params["order"][:address_id]
     params["order"].delete(:total)
+
     @order = Order.new(order_params)
-    @order.add_line_items_from_cart(@cart)
+    # @order.add_line_items_from_cart(@cart)
     
     if params[:commit] == 'Stripe'
       redirect_to new_charge_path(:total => total) 
     elsif params[:commit] == 'Paypal'
       redirect_to new_paypal_path
     else params[:commit] == 'Cash On Delivery'
+      
       respond_to do |format|
-         @order.save
+
+        if @order.save
           Cart.destroy(session[:cart_id])
           session[:cart_id] = nil
-  
+
           format.html { redirect_to root_url, notice: "Thankyou for your order" }
           format.json { render :show, status: :created, location: @order }
-        # else
-        #   format.html { render :new, status: :unprocessable_entity }
-        #   format.json { render json: @order.errors, status: :unprocessable_entity }
-        # end
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @order.errors, status: :unprocessable_entity }
+        end
       end
     end
-    
-    # respond_to do |format|
-    #   if @order.save
-    #     Cart.destroy(session[:cart_id])
-    #     session[:cart_id] = nil
-
-    #     format.html { redirect_to root_url, notice: "Thankyou for your order" }
-    #     format.json { render :show, status: :created, location: @order }
-    #   else
-    #     format.html { render :new, status: :unprocessable_entity }
-    #     format.json { render json: @order.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
 
   # PATCH/PUT /orders/1 or /orders/1.json
@@ -97,6 +89,6 @@ class OrdersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def order_params
-      params.require(:order).permit(:address_id)
+      params.require(:order).permit(:address_id,:total)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_08_060201) do
+ActiveRecord::Schema.define(version: 2022_06_09_053935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,9 +113,6 @@ ActiveRecord::Schema.define(version: 2022_06_08_060201) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "paytype"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "address_id"
